c118f48cac66f8cc4280445b24712414fca30382
Kurt Greaves <kurt.greaves@zepben.com>
2020-09-11 06:14:11 +0000

    Merged in fix-changeset20 (pull request #5)
    Drop constraint before applying sequence number updates

--- src/main/java/com/zepben/cimbend/database/sqlite/upgrade/ChangeSet20.kt
+++ src/main/java/com/zepben/cimbend/database/sqlite/upgrade/ChangeSet20.kt
@@ -20,8 +20,10 @@ package com.zepben.cimbend.database.sqlite.upgrade
 internal fun changeSet20() = ChangeSet(20) {
     listOf(
         "DROP INDEX power_transformer_ends_power_transformer_mrid_end_number",
+        "DROP INDEX terminals_conducting_equipment_mrid_sequence_number",
         "UPDATE terminals SET sequence_number = sequence_number + 1",
         "UPDATE power_transformer_ends SET end_number = end_number + 1",
-        "CREATE UNIQUE INDEX power_transformer_ends_power_transformer_mrid_end_number ON power_transformer_ends (power_transformer_mrid, end_number)"
+        "CREATE UNIQUE INDEX power_transformer_ends_power_transformer_mrid_end_number ON power_transformer_ends (power_transformer_mrid, end_number)",
+        "CREATE UNIQUE INDEX terminals_conducting_equipment_mrid_sequence_number ON terminals (conducting_equipment_mrid, sequence_number)"
     )
 }

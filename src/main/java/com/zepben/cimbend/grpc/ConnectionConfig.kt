1661fbaf55a6225bc2c5dc1d7bd4547c7b08b64e
Anthony Charlton <anthony.charlton@zepben.com>
2020-10-22 15:44:13 +1100

    EVO-786 - Changed TLS to be disabled by default.
    Signed-off-by: Anthony Charlton <anthony.charlton@zepben.com>

--- src/main/java/com/zepben/cimbend/grpc/ConnectionConfig.kt
+++ src/main/java/com/zepben/cimbend/grpc/ConnectionConfig.kt
@@ -12,7 +12,7 @@ package com.zepben.cimbend.grpc
  *
  * @property host The host of the producer server
  * @property port The port the producer server is listening on
- * @property enableTls Indicates if TLS should be enabled for the connection [default: true].
+ * @property enableTls Indicates if TLS should be enabled for the connection [default: false].
  * @property trustCertPath The path of the trust certificate that signed the server certificate, null to use system default [default: use system default].
  * @property authCertPath The path of the client certificate to use for authentication (must be signed by the servers authentication trust certificate). Use null to disable [default: disabled].
  * @property authKeyPath The path of the private key for the client authentication certificate if one is specified, otherwise ignored.
@@ -20,7 +20,7 @@ package com.zepben.cimbend.grpc
 data class ConnectionConfig(
     val host: String,
     val port: Int,
-    val enableTls: Boolean = true,
+    val enableTls: Boolean = false,
     val trustCertPath: String? = null,
     val authCertPath: String? = null,
     val authKeyPath: String? = null

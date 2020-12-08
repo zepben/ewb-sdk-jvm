29f67b1098015e316fc479a729865de7e9d0a051
Anthony Charlton <anthony.charlton@zepben.com>
2020-12-08 18:02:21 +1100

    EP-82 - Renaming database packages
    Signed-off-by: Anthony Charlton <anthony.charlton@zepben.com>

--- src/main/java/com/zepben/evolve/database/sqlite/writers/MeasurementServiceWriter.kt
+++ src/main/java/com/zepben/evolve/database/sqlite/writers/MeasurementServiceWriter.kt
@@ -5,13 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-package com.zepben.cimbend.database.sqlite.writers
-
-import com.zepben.cimbend.cim.iec61970.base.meas.Control
-import com.zepben.cimbend.cim.iec61970.base.meas.Measurement
-import com.zepben.cimbend.cim.iec61970.base.scada.RemoteControl
-import com.zepben.cimbend.cim.iec61970.base.scada.RemoteSource
-import com.zepben.cimbend.measurement.MeasurementService
+package com.zepben.evolve.database.sqlite.writers
 
 class MeasurementServiceWriter(hasCommon: (String) -> Boolean, addCommon: (String) -> Boolean) {
 

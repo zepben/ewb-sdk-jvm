12879dbbeebd5493adcfffd9b48f34ebefa6e653
Anthony Charlton <anthony.charlton@zepben.com>
2021-05-31 11:54:03 +1000

    PROJ-524 - Removed hierarchy class check and now always get the hierarchy for anything with references (as it pulls in the hierarchy in almost all cases anyway)
    Signed-off-by: Anthony Charlton <anthony.charlton@zepben.com>

--- src/test/kotlin/com/zepben/evolve/streaming/get/testdata/LoopNetwork.kt
+++ src/test/kotlin/com/zepben/evolve/streaming/get/testdata/LoopNetwork.kt
@@ -9,7 +9,9 @@
 package com.zepben.evolve.streaming.get.testdata
 
 import com.zepben.evolve.cim.iec61970.base.core.BaseVoltage
+import com.zepben.evolve.cim.iec61970.base.core.Feeder
 import com.zepben.evolve.cim.iec61970.base.core.Substation
+import com.zepben.evolve.cim.iec61970.base.core.Terminal
 import com.zepben.evolve.cim.iec61970.base.wires.Junction
 import com.zepben.evolve.cim.iec61970.infiec61970.feeder.Circuit
 import com.zepben.evolve.cim.iec61970.infiec61970.feeder.Loop
@@ -24,8 +26,10 @@ object LoopNetwork {
     // BTS        ZTS
     // ||\__ ACT _/
     // |\________
-    // | /--.--\ |
-    // ZEP  |  BEN
+    // |         |
+    // ZEP     BEN
+    //   \--.--/
+    //      |
     //     CBR
     //
     fun create() = NetworkService().apply {
@@ -59,15 +63,38 @@ object LoopNetwork {
     private fun NetworkService.createSubstation(id: String, vararg voltages: BaseVoltage) =
         Substation(id).apply {
             name = id
-            voltages.forEach { addEquipment(Junction().apply { baseVoltage = it }) }
+            voltages.forEach {
+                addEquipment(createJunction("$id-j-${it.nominalVoltage}", it))
+                if (it.nominalVoltage == 11000)
+                    createFeeder("${id}001", this, it)
+            }
         }.also {
+            add(it)
+        }
+
+    private fun NetworkService.createJunction(id: String, voltage: BaseVoltage) =
+        Junction(id).apply {
+            name = id
+            baseVoltage = voltage
+        }.also {
+            it.addTerminal(Terminal("$id-t").apply { conductingEquipment = it; add(this) })
+            add(it)
+        }
+
+    private fun NetworkService.createFeeder(id: String, substation: Substation, voltage: BaseVoltage) =
+        Feeder(id).apply {
+            name = id
+            addEquipment(createJunction("$id-j", voltage))
+        }.also {
+            it.normalEnergizingSubstation = substation
+            substation.addFeeder(it)
             add(it)
         }
 
     private fun NetworkService.createCircuit(id: String, voltage: BaseVoltage, vararg endSubstations: Substation) =
         Circuit(id).apply {
             name = id
-            addEquipment(Junction().apply { baseVoltage = voltage })
+            addEquipment(createJunction("$id-j", voltage))
         }.also {
             endSubstations.forEach { substation ->
                 it.addEndSubstation(substation)

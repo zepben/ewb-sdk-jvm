61b67e132ecadd976eeb44956f761f542e58ea09
Jimmy Tung <jimmy.tung@zepben.com>
2021-12-10 08:53:39 +1100

    removal CimProducerClient and all mention
    Signed-off-by: Jimmy Tung <jimmy.tung@zepben.com>

--- src/test/kotlin/com/zepben/evolve/streaming/grpc/GrpcChannelFactoryTest.kt
+++ src/test/kotlin/com/zepben/evolve/streaming/grpc/GrpcChannelFactoryTest.kt
@@ -1,15 +1,12 @@
 /*
- * Copyright 2020 Zeppelin Bend Pty Ltd
+ * Copyright 2021 Zeppelin Bend Pty Ltd
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-package com.zepben.evolve.streaming.put.grpc
+package com.zepben.evolve.streaming.grpc
 
-import com.zepben.evolve.streaming.grpc.BadConfigException
-import com.zepben.evolve.streaming.grpc.ConnectionConfig
-import com.zepben.evolve.streaming.grpc.GrpcChannelFactory
 import com.zepben.testutils.exception.ExpectException.expect
 import org.junit.jupiter.api.Test
 

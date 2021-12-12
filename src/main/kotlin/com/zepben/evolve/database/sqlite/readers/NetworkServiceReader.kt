cbe0021bd9e42035fcf281faf998e78055bdad03
Jimmy Tung <jimmy.tung@zepben.com>
2021-12-13 10:36:54 +1100

    change load sequencing to fix test error
    Signed-off-by: Jimmy Tung <jimmy.tung@zepben.com>

--- src/main/kotlin/com/zepben/evolve/database/sqlite/readers/NetworkServiceReader.kt
+++ src/main/kotlin/com/zepben/evolve/database/sqlite/readers/NetworkServiceReader.kt
@@ -75,8 +75,6 @@ class NetworkServiceReader constructor(getStatement: () -> Statement) : BaseServ
         status = status and loadEach<TableEquivalentBranches>("equivalent branches", reader::load)
         status = status and loadEach<TablePowerElectronicsConnection>("power electronics connection", reader::load)
         status = status and loadEach<TablePowerElectronicsConnectionPhases>("power electronics connection phases", reader::load)
-        status = status and loadEach<TablePowerElectronicsConnectionPECPhases>("power electronics connection to power electronics connection phases association", reader::load)
-        status = status and loadEach<TablePowerElectronicsConnectionPEUnits>("power electronics connection to power electronics units association", reader::load)
         status = status and loadEach<TableBatteryUnit>("battery unit", reader::load)
         status = status and loadEach<TablePhotoVoltaicUnit>("photo voltaic unit", reader::load)
         status = status and loadEach<TablePowerElectronicsWindUnit>("power electronics wind unit", reader::load)
@@ -119,6 +117,8 @@ class NetworkServiceReader constructor(getStatement: () -> Statement) : BaseServ
         status = status and loadEach<TableAnalogs>("analogs", reader::load)
         status = status and loadEach<TableAccumulators>("accumulators", reader::load)
         status = status and loadEach<TableDiscretes>("discretes", reader::load)
+        status = status and loadEach<TablePowerElectronicsConnectionPECPhases>("power electronics connection to power electronics connection phases association", reader::load)
+        status = status and loadEach<TablePowerElectronicsConnectionPEUnits>("power electronics connection to power electronics units association", reader::load)
 
         status = status and loadNames(reader)
 

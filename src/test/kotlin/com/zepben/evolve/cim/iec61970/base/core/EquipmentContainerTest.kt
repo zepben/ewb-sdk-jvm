1e68ad5a7220485e4c97075cd9e0c8bc7ea1ad4b
Marcus Koh <marcus.koh@zepben.com>
2022-08-01 11:24:21 +1000

    Test convenience functions
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/test/kotlin/com/zepben/evolve/cim/iec61970/base/core/EquipmentContainerTest.kt
+++ src/test/kotlin/com/zepben/evolve/cim/iec61970/base/core/EquipmentContainerTest.kt
@@ -10,10 +10,10 @@ package com.zepben.evolve.cim.iec61970.base.core
 import com.zepben.evolve.utils.PrivateCollectionValidator
 import com.zepben.testutils.junit.SystemLogExtension
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
-import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
+import org.mockito.kotlin.eq
 
 internal class EquipmentContainerTest {
 
@@ -39,5 +39,35 @@ internal class EquipmentContainerTest {
             EquipmentContainer::removeEquipment,
             EquipmentContainer::clearEquipment
         )
+    }
+
+    @Test
+    internal fun normalFeeders() {
+        val fdr1 = Feeder()
+        val fdr2 = Feeder()
+        val fdr3 = Feeder()
+
+        val eq1 = object : Equipment() {}.addContainer(fdr1).addContainer(fdr2)
+        val eq2 = object : Equipment() {}.addContainer(fdr2).addContainer(fdr3)
+
+        val equipmentContainer = object : EquipmentContainer() {}.addEquipment(eq1).addEquipment(eq2)
+
+        assertThat(equipmentContainer.normalFeeders(), containsInAnyOrder(fdr1, fdr2, fdr3))
+        assertThat(equipmentContainer.currentFeeders(), empty())
+    }
+
+    @Test
+    internal fun currentFeeders() {
+        val fdr1 = Feeder()
+        val fdr2 = Feeder()
+        val fdr3 = Feeder()
+
+        val eq1 = object : Equipment() {}.addCurrentContainer(fdr1).addCurrentContainer(fdr2)
+        val eq2 = object : Equipment() {}.addCurrentContainer(fdr2).addCurrentContainer(fdr3)
+
+        val equipmentContainer = object : EquipmentContainer() {}.addEquipment(eq1).addEquipment(eq2)
+
+        assertThat(equipmentContainer.currentFeeders(), containsInAnyOrder(fdr1, fdr2, fdr3))
+        assertThat(equipmentContainer.normalFeeders(), empty())
     }
 }

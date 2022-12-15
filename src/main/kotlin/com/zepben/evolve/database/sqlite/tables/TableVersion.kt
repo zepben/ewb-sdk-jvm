d0df19a1a581d11df47c95c83e311f1cc27982c8
Marcus Koh <marcus.koh@zepben.com>
2022-12-15 19:20:08 +1100

    Add changeset 45 (TODO: validator)
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/main/kotlin/com/zepben/evolve/database/sqlite/tables/TableVersion.kt
+++ src/main/kotlin/com/zepben/evolve/database/sqlite/tables/TableVersion.kt
@@ -14,7 +14,7 @@ import com.zepben.evolve.database.sqlite.tables.Column.Nullable.NOT_NULL
 @EverythingIsNonnullByDefault
 class TableVersion : SqliteTable() {
 
-    val SUPPORTED_VERSION = 44
+    val SUPPORTED_VERSION = 45
 
     val VERSION = Column(++columnIndex, "version", "TEXT", NOT_NULL)
 
---
+++ src/test/kotlin/com/zepben/evolve/database/sqlite/upgrade/changesets/ChangeSet45Validator.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 Zeppelin Bend Pty Ltd
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.zepben.evolve.database.sqlite.upgrade.changesets
+
+import java.sql.Statement
+
+object ChangeSet45Validator : ChangeSetValidator {
+    override fun setUpStatements(): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun populateStatements(): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun validate(statement: Statement) {
+        TODO("Not yet implemented")
+    }
+
+    override fun tearDownStatements(): List<String> {
+        TODO("Not yet implemented")
+    }
+
+}

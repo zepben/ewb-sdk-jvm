b2e67643120cae8a952368e553a5964c809861c4
Marcus Koh <marcus.koh@zepben.com>
2024-02-05 15:42:29 +1100

    Add missing tests for NS comparator
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/test/kotlin/com/zepben/evolve/services/network/NetworkServiceComparatorTest.kt
+++ src/test/kotlin/com/zepben/evolve/services/network/NetworkServiceComparatorTest.kt
@@ -33,6 +33,8 @@ import com.zepben.evolve.cim.iec61970.base.wires.*
 import com.zepben.evolve.cim.iec61970.base.wires.generation.production.*
 import com.zepben.evolve.cim.iec61970.infiec61970.feeder.Circuit
 import com.zepben.evolve.cim.iec61970.infiec61970.feeder.Loop
+import com.zepben.evolve.cim.iec61970.infiec61970.protection.PowerDirectionKind
+import com.zepben.evolve.cim.iec61970.infiec61970.protection.ProtectionKind
 import com.zepben.evolve.cim.iec61970.infiec61970.wires.generation.production.EvChargingUnit
 import com.zepben.evolve.services.common.*
 import com.zepben.evolve.services.network.tracing.feeder.FeederDirection

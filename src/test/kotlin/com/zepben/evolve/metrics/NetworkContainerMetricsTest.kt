08e93782c367e96309901c615e43c75273db1eb8
Marcus Koh <marcus.koh@zepben.com>
2024-07-22 11:35:39 +1000

    import hamcrest functions
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/test/kotlin/com/zepben/evolve/metrics/NetworkContainerMetricsTest.kt
+++ src/test/kotlin/com/zepben/evolve/metrics/NetworkContainerMetricsTest.kt
@@ -8,8 +8,8 @@
 
 package com.zepben.evolve.metrics
 
-import org.hamcrest.MatcherAssert
-import org.hamcrest.Matchers
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
 
 internal class NetworkContainerMetricsTest {
@@ -17,22 +17,22 @@ internal class NetworkContainerMetricsTest {
     @Test
     internal fun increaseMissing() {
         val ncMetrics = mutableMapOf<String, Double>()
-        MatcherAssert.assertThat(ncMetrics.increase("a"), Matchers.equalTo(1.0))
-        MatcherAssert.assertThat(ncMetrics.increase("b", 2.5), Matchers.equalTo(2.5))
-        MatcherAssert.assertThat(ncMetrics.increase("c", 0), Matchers.equalTo(0.0))
-        MatcherAssert.assertThat(ncMetrics.increase("d", -3.3), Matchers.equalTo(-3.3))
-        MatcherAssert.assertThat(ncMetrics, Matchers.equalTo(mapOf("a" to 1.0, "b" to 2.5, "c" to 0.0, "d" to -3.3)))
+        assertThat(ncMetrics.increase("a"), equalTo(1.0))
+        assertThat(ncMetrics.increase("b", 2.5), equalTo(2.5))
+        assertThat(ncMetrics.increase("c", 0), equalTo(0.0))
+        assertThat(ncMetrics.increase("d", -3.3), equalTo(-3.3))
+        assertThat(ncMetrics, equalTo(mapOf("a" to 1.0, "b" to 2.5, "c" to 0.0, "d" to -3.3)))
     }
 
     @Test
     internal fun increaseExisting() {
         val ncMetrics = mutableMapOf("a" to 1.0)
-        MatcherAssert.assertThat(ncMetrics.increase("a"), Matchers.equalTo(2.0))
-        MatcherAssert.assertThat(ncMetrics, Matchers.equalTo(mapOf("a" to 2.0)))
-        MatcherAssert.assertThat(ncMetrics.increase("a", -2), Matchers.equalTo(0.0))
-        MatcherAssert.assertThat(ncMetrics, Matchers.equalTo(mapOf("a" to 0.0)))
-        MatcherAssert.assertThat(ncMetrics.increase("a", 1.5), Matchers.equalTo(1.5))
-        MatcherAssert.assertThat(ncMetrics, Matchers.equalTo(mapOf("a" to 1.5)))
+        assertThat(ncMetrics.increase("a"), equalTo(2.0))
+        assertThat(ncMetrics, equalTo(mapOf("a" to 2.0)))
+        assertThat(ncMetrics.increase("a", -2), equalTo(0.0))
+        assertThat(ncMetrics, equalTo(mapOf("a" to 0.0)))
+        assertThat(ncMetrics.increase("a", 1.5), equalTo(1.5))
+        assertThat(ncMetrics, equalTo(mapOf("a" to 1.5)))
     }
 
     @Test
@@ -40,7 +40,7 @@ internal class NetworkContainerMetricsTest {
         val ncMetrics = mutableMapOf("a" to 1.5)
         ncMetrics["a"] = 3
         ncMetrics["b"] = 5
-        MatcherAssert.assertThat(ncMetrics, Matchers.equalTo(mapOf("a" to 3.0, "b" to 5.0)))
+        assertThat(ncMetrics, equalTo(mapOf("a" to 3.0, "b" to 5.0)))
     }
 
 }

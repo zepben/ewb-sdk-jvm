7c37aa459c8e2f4a382450d41285ecad66081092
Glenn Carwardine <glenn.carwardine@zepben.com>
2024-09-12 14:13:43 +1000

    Rework of a bunch of NetworkTrace/Traversal stuff based on feedback: * Network trace is now always backed by the same terminal queue next. * Network trace has the ability to only "action" conducting equipment so not all steps are actioned because of the above change and only wanted to "step" on conducing equipment. * Fixed an issue where if there are multiple start items on a branching traversal, the start items should all start on their own branch.
    Signed-off-by: Glenn Carwardine <glenn.carwardine@zepben.com>

--- src/main/kotlin/com/zepben/evolve/services/network/tracing/tree/DownstreamTree.kt
+++ src/main/kotlin/com/zepben/evolve/services/network/tracing/tree/DownstreamTree.kt
@@ -11,7 +11,8 @@ package com.zepben.evolve.services.network.tracing.tree
 import com.zepben.evolve.cim.iec61970.base.core.ConductingEquipment
 import com.zepben.evolve.services.network.tracing.OpenTest
 import com.zepben.evolve.services.network.tracing.feeder.DirectionSelector
-import com.zepben.evolve.services.network.tracing.feeder.FeederDirection
+import com.zepben.evolve.services.network.tracing.networktrace.Conditions.downstream
+import com.zepben.evolve.services.network.tracing.networktrace.Conditions.stopAtOpen
 import com.zepben.evolve.services.network.tracing.networktrace.NetworkTrace
 import com.zepben.evolve.services.network.tracing.networktrace.NetworkTraceStep
 import com.zepben.evolve.services.network.tracing.networktrace.StepPath
@@ -20,34 +21,30 @@ import com.zepben.evolve.services.network.tracing.traversalV2.StepContext
 import com.zepben.evolve.services.network.tracing.traversalV2.WeightedPriorityQueue
 
 class DownstreamTree(
-    private val openTest: OpenTest,
-    private val directionSelector: DirectionSelector
+    openTest: OpenTest,
+    directionSelector: DirectionSelector
 ) {
 
     private val traversal: NetworkTrace<TreeNode> = Tracing.connectedEquipmentTrace(
         { WeightedPriorityQueue.processQueue { it.data.sortWeight } },
         { WeightedPriorityQueue.branchQueue { it.data.sortWeight } },
-        computeNextT = this::createNextTreeNode
+        computeNextT = ::createNextTreeNode
     )
-        .addQueueCondition(this::canQueue)
-        .addStepAction(this::addItemAsChild)
+        .addConditions(downstream(directionSelector), stopAtOpen(openTest))
+        .addStepAction(::addItemAsChild)
 
     fun run(start: ConductingEquipment): TreeNode {
         val root = TreeNode(start, null)
-        traversal.run(root.conductingEquipment, false, root)
+        traversal.run(root.conductingEquipment, root, false)
         return root
     }
 
-    private fun createNextTreeNode(currentItem: NetworkTraceStep<TreeNode>, context: StepContext, nextPath: StepPath): TreeNode {
-        // We don't add the new tree node as a child here as we may not actually visit it based on other conditions.
+    @Suppress("UNUSED_PARAMETER")
+    private fun createNextTreeNode(currentItem: NetworkTraceStep<TreeNode>, unused: StepContext, nextPath: StepPath): TreeNode {
         return TreeNode(nextPath.toEquipment, currentItem.data)
     }
 
-    private fun canQueue(item: NetworkTraceStep<TreeNode>, context: StepContext): Boolean {
-        val fromTerminal = item.path.fromTerminal ?: return false
-        return !openTest.isOpen(item.path.fromEquipment, null) && FeederDirection.DOWNSTREAM in directionSelector.select(fromTerminal).value
-    }
-
+    @Suppress("UNUSED_PARAMETER")
     private fun addItemAsChild(item: NetworkTraceStep<TreeNode>, context: StepContext) {
         // If we visit a node, we add it as a child to its parent
         item.data.parent?.addChild(item.data)

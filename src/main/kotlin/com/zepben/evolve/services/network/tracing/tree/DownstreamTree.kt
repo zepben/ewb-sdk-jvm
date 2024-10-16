43a0ee44022268c3f05262db528156e7c02beed5
Glenn Carwardine <glenn.carwardine@zepben.com>
2024-10-16 11:32:03 +1100

    NetworkTrace now needs a NetworkStateOperators and queueing next checks in inService flag of conducting equipment. Also added some QOL around adding network based trace conditions now that the NetworkTrace owns a NetworkStateOperators.
    Signed-off-by: Glenn Carwardine <glenn.carwardine@zepben.com>

--- src/main/kotlin/com/zepben/evolve/services/network/tracing/tree/DownstreamTree.kt
+++ src/main/kotlin/com/zepben/evolve/services/network/tracing/tree/DownstreamTree.kt
@@ -9,29 +9,24 @@
 package com.zepben.evolve.services.network.tracing.tree
 
 import com.zepben.evolve.cim.iec61970.base.core.ConductingEquipment
-import com.zepben.evolve.cim.iec61970.base.core.Terminal
-import com.zepben.evolve.services.network.tracing.feeder.FeederDirection
+import com.zepben.evolve.services.network.tracing.networktrace.*
 import com.zepben.evolve.services.network.tracing.networktrace.Conditions.downstream
-import com.zepben.evolve.services.network.tracing.networktrace.NetworkTrace
-import com.zepben.evolve.services.network.tracing.networktrace.NetworkTraceStep
-import com.zepben.evolve.services.network.tracing.networktrace.StepPath
-import com.zepben.evolve.services.network.tracing.networktrace.Tracing
 import com.zepben.evolve.services.network.tracing.traversalV2.WeightedPriorityQueue
 
 
 class DownstreamTree(
-    getDirection: (Terminal) -> FeederDirection
+    networkStateOperators: NetworkStateOperators
 ) {
 
     private val traversal: NetworkTrace<TreeNode> = Tracing.connectedEquipmentTrace(
-        { WeightedPriorityQueue.processQueue { it.data.sortWeight } },
-        { WeightedPriorityQueue.branchQueue { it.data.sortWeight } },
+        networkStateOperators = networkStateOperators,
+        queueFactory = { WeightedPriorityQueue.processQueue { it.data.sortWeight } },
+        branchQueueFactory = { WeightedPriorityQueue.branchQueue { it.data.sortWeight } },
         computeNextT = { currentItem: NetworkTraceStep<TreeNode>, _, nextPath: StepPath ->
             TreeNode(nextPath.toEquipment, currentItem.data)
         }
     )
-        // TODO: Can we remove open test?
-        .addConditions(downstream(getDirection))
+        .addNetworkCondition { downstream() }
         .addStepAction { (_, treeNode), _ ->
             // If we visit a node, we add it as a child to its parent
             treeNode.parent?.addChild(treeNode)

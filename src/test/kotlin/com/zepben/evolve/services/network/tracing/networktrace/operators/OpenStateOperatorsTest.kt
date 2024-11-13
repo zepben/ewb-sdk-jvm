975422eb26e5a35464e95c287d7a8dc0fc155fc5
Glenn Carwardine <glenn.carwardine@zepben.com>
2024-11-13 11:36:22 +1100

    Update copyright headers. [skip ci]
    Signed-off-by: Glenn Carwardine <glenn.carwardine@zepben.com>

--- src/test/kotlin/com/zepben/evolve/services/network/tracing/networktrace/operators/OpenStateOperatorsTest.kt
+++ src/test/kotlin/com/zepben/evolve/services/network/tracing/networktrace/operators/OpenStateOperatorsTest.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2024 Zeppelin Bend Pty Ltd
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.zepben.evolve.services.network.tracing.networktrace.operators
 
 import com.zepben.evolve.cim.iec61970.base.wires.SinglePhaseKind

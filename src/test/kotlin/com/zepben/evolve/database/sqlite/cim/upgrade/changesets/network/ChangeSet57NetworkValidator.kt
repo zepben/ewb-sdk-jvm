cdeae829012715ea192f8d82a397d124ca9038cc
Jimmy Tung <jimmy.tung@zepben.com>
2024-11-29 17:08:11 +1100

    Remove Association tables that are not required.
    Signed-off-by: Jimmy Tung <jimmy.tung@zepben.com>

--- src/test/kotlin/com/zepben/evolve/database/sqlite/cim/upgrade/changesets/network/ChangeSet57NetworkValidator.kt
+++ src/test/kotlin/com/zepben/evolve/database/sqlite/cim/upgrade/changesets/network/ChangeSet57NetworkValidator.kt
@@ -27,8 +27,6 @@ object ChangeSet57NetworkValidator : ChangeSetValidator(DatabaseType.NETWORK_MOD
         "INSERT INTO battery_controls (mrid, name, description, num_diagram_objects, location_mrid, num_controls, discrete, mode, monitored_phase, target_deadband, target_value, enabled, max_allowed_target_value, min_allowed_target_value, rated_current, terminal_mrid, ct_primary, min_target_deadband, battery_unit_mrid, charging_Rate, discharging_rate, reserve_percent, control_mode) VALUES ('mrid', 'name', 'description', 1, 'location_mrid', 2, true, 'mode', 'monitored_phase', 3.0, 4.0, true, 5.0, 6.0, 7.0, 8.0, 9.0, 'terminal_mrid', 'battery_unit_mrid', 10.0, 11.0, 12.0, 'control_mode');",
         "INSERT INTO pan_demand_response_functions (mrid, name, description, num_diagram_objects, end_device_mrid, enabled, kind, appliance) VALUES ('mrid', 'name', 'description', 1, 'end_device_mrid', true, 'kind', 2);",
         "INSERT INTO static_var_compensators (mrid, name, description, num_diagram_objects, location_mrid, num_controls, normally_in_service, in_service, commissioned_date, base_voltage_mrid, control_enabled, regulating_control_mrid, capacitive_rating, inductive_rating, q, svc_control_mode, voltage_set_point) VALUES ('mrid', 'name', 'description', 1, 'location_mrid', 2, true, true, 'commissioned_date', 'base_voltage_mrid', true, 'regulating_control_mrid', 3.0, 4.0, 5.0, 'svc_control_mode', 6);",
-        "INSERT INTO battery_units_battery_controls (battery_unit_mrid, battery_control_mrid) VALUES ('battery_unit_mrid', 'battery_control_mrid');",
-        "INSERT INTO end_devices_end_device_functions (end_device_mrid, end_device_function_mrid) VALUES ('end_device_mrid', 'end_device_function_mrid');",
     )
 
     override fun validateChanges(statement: Statement) {
@@ -37,8 +35,6 @@ object ChangeSet57NetworkValidator : ChangeSetValidator(DatabaseType.NETWORK_MOD
         ensureAddedBatteryControls(statement)
         ensureAddedPanDemandResponseFunctions(statement)
         ensureAddedStaticVarCompensators(statement)
-        ensureAddedBatteryUnitsBatteryControls(statement)
-        ensureAddedEndDevicesEndDeviceFunctions(statement)
     }
 
     override fun tearDownStatements(): List<String> =
@@ -46,8 +42,7 @@ object ChangeSet57NetworkValidator : ChangeSetValidator(DatabaseType.NETWORK_MOD
             "DELETE FROM battery_controls;",
             "DELETE FROM pan_demand_response_functions;",
             "DELETE FROM static_var_compensators;",
-            "DELETE FROM battery_units_battery_controls;",
-            "DELETE FROM end_devices_end_device_functions;"
+            "DELETE FROM tap_changer_controls;"
         )
 
     private fun ensureAddedBatteryControls(statement: Statement) {
@@ -63,26 +58,6 @@ object ChangeSet57NetworkValidator : ChangeSetValidator(DatabaseType.NETWORK_MOD
     private fun ensureAddedStaticVarCompensators(statement: Statement) {
         ensureTables(statement, "static_var_compensators")
         ensureIndexes(statement, "static_var_compensators_mrid")
-    }
-
-    private fun ensureAddedBatteryUnitsBatteryControls(statement: Statement) {
-        ensureTables(statement, "battery_units_battery_controls")
-        ensureIndexes(
-            statement,
-            "battery_units_mrid_battery_controls_mrid",
-            "battery_units_battery_controls_battery_unit_mrid",
-            "battery_units_battery_controls_battery_control_mrid"
-        )
-    }
-
-    private fun ensureAddedEndDevicesEndDeviceFunctions(statement: Statement) {
-        ensureTables(statement, "end_devices_end_device_functions")
-        ensureIndexes(
-            statement,
-            "battery_units_mrid_battery_controls_mrid",
-            "end_devices_end_device_functions_end_device_mrid",
-            "end_devices_end_device_functions_end_device_function_mrid"
-        )
     }
 
 }

2bc790682c60d5fd36511e8e24806402d3c47c16
Glenn Carwardine <glenn.carwardine@zepben.com>
2025-01-16 22:33:35 +1100

    Added missing copyright headers.
    Signed-off-by: Glenn Carwardine <glenn.carwardine@zepben.com>

--- src/test/kotlin/com/zepben/evolve/services/network/tracing/networktrace/conditions/DirectionConditionTest.kt
+++ src/test/kotlin/com/zepben/evolve/services/network/tracing/networktrace/conditions/DirectionConditionTest.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2025 Zeppelin Bend Pty Ltd
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.zepben.evolve.services.network.tracing.networktrace.conditions
 
 import com.zepben.evolve.cim.iec61970.base.core.Terminal

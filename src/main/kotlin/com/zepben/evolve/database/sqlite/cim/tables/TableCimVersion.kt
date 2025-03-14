99029322b96cd3269d87babc7a2b68743d28c316
Kurt Greaves <kurt.greaves@zepben.com>
2025-03-14 11:58:41 +1100

    Add Asset to PowerSystemResource relationship
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- src/main/kotlin/com/zepben/evolve/database/sqlite/cim/tables/TableCimVersion.kt
+++ src/main/kotlin/com/zepben/evolve/database/sqlite/cim/tables/TableCimVersion.kt
@@ -13,4 +13,4 @@ import com.zepben.evolve.database.sqlite.common.SqliteTableVersion
 /**
  * The `version` table in the CIM databases. Increment this when doing a schema update.
  */
-val tableCimVersion: SqliteTableVersion = SqliteTableVersion(58)
+val tableCimVersion: SqliteTableVersion = SqliteTableVersion(59)

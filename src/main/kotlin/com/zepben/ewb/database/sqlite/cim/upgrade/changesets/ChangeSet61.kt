0199383c04918b2cd21265bb5b9ff1ce2abc0367
Max Chesterfield <max.chesterfield@zepben.com>
2025-08-26 16:57:47 +1000

    code gen, how we loveth thee
    Signed-off-by: Max Chesterfield <max.chesterfield@zepben.com>

--- src/main/kotlin/com/zepben/ewb/database/sqlite/cim/upgrade/changesets/ChangeSet61.kt
+++ src/main/kotlin/com/zepben/ewb/database/sqlite/cim/upgrade/changesets/ChangeSet61.kt
@@ -8,9 +8,13 @@
 
 package com.zepben.ewb.database.sqlite.cim.upgrade.changesets
 
+import com.zepben.ewb.cim.iec61968.common.StreetDetail
 import com.zepben.ewb.database.paths.DatabaseType
+import com.zepben.ewb.database.sqlite.cim.tables.iec61970.base.wires.TableTransformerEnds
+import com.zepben.ewb.database.sqlite.cim.tables.iec61970.base.wires.TableTransformerStarImpedances
 import com.zepben.ewb.database.sqlite.cim.upgrade.Change
 import com.zepben.ewb.database.sqlite.cim.upgrade.ChangeSet
+import com.zepben.ewb.database.sqlite.common.SqliteTable
 import kotlin.collections.plus
 
 internal fun changeSet61() = ChangeSet(
@@ -27,33 +31,117 @@ internal fun changeSet61() = ChangeSet(
 
 @Suppress("ObjectPropertyName")
 private val `retype nonnull columns to null network` = Change(
-    powerSystemResource("ac_line_segments")
-        + pole("poles")
-        + usagePoint("usage_points")
-        + nameType("name_types")
-        + analog("analogs")
-        + energyConsumer("energy_consumers")
-        + energySource("energy_sources")
-        + regulatingCondEq()
+    powerSystemResource("ac_line_segments") +
+        identifiedObject("accumulators") +
+        analog("analogs") +
+        identifiedObject("asset_owners") +
+        identifiedObject("base_voltages") +
+        powerSystemResource("battery_controls") +
+        powerSystemResource("battery_units") +
+        powerSystemResource("breakers") +
+        powerSystemResource("busbar_sections") +
+        identifiedObject("cable_info") +
+        powerSystemResource("circuits") +
+        powerSystemResource("clamps") +
+        identifiedObject("connectivity_nodes") +
+        identifiedObject("controls") +
+        powerSystemResource("current_relays") +
+        identifiedObject("current_transformer_info") +
+        powerSystemResource("current_transformers") +
+        document("customer_agreements") +
+        identifiedObject("customers") +
+        powerSystemResource("cuts") +
+        identifiedObject("diagram_objects") +
+        identifiedObject("diagrams") +
+        powerSystemResource("disconnectors") +
+        identifiedObject("discretes") +
+        powerSystemResource("distance_relays") +
+        powerSystemResource("energy_consumer_phases") +
+        energyConsumer("energy_consumers") +
+        powerSystemResource("energy_source_phases") +
+        energySource("energy_sources") +
+        powerSystemResource("equivalent_branches") +
+        powerSystemResource("ev_charging_units") +
+        powerSystemResource("fault_indicators") +
+        powerSystemResource("feeders") +
+        powerSystemResource("fuses") +
+        identifiedObject("geographical_regions") +
+        powerSystemResource("ground_disconnectors") +
+        powerSystemResource("grounding_impedances") +
+        powerSystemResource("grounds") +
+        powerSystemResource("jumpers") +
+        powerSystemResource("junctions") +
+        shuntCompensator("linear_shunt_compensators") +
+        powerSystemResource("load_break_switches") +
+        streetAddress("location_street_addresses") +
+        identifiedObject("locations") +
+        identifiedObject("loops") +
+        powerSystemResource("lv_feeders") +
+        identifiedObject("meters") +
+        nameType("name_types") +
+        identifiedObject("no_load_tests") +
+        identifiedObject("open_circuit_tests") +
+        document("operational_restrictions") +
+        identifiedObject("organisations") +
+        identifiedObject("overhead_wire_info") +
+        identifiedObject("pan_demand_response_functions") +
+        identifiedObject("per_length_phase_impedances") +
+        identifiedObject("per_length_sequence_impedances") +
+        powerSystemResource("petersen_coils") +
+        powerSystemResource("photo_voltaic_units") +
+        identifiedObject("poles") +
+        identifiedObject("potential_transformer_info") +
+        powerSystemResource("potential_transformers") +
+        powerSystemResource("power_electronics_connection_phases") +
+        regulatingCondEq("power_electronics_connections") +
+        powerSystemResource("power_electronics_wind_units") +
+        transformerEnd("power_transformer_ends") +
+        identifiedObject("power_transformer_info") +
+        powerSystemResource("power_transformers") +
+        document("pricing_structures") +
+        identifiedObject("protection_relay_schemes") +
+        powerSystemResource("protection_relay_systems") +
+        tapChanger("ratio_tap_changers") +
+        identifiedObject("reactive_capability_curves") +
+        powerSystemResource("reclosers") +
+        identifiedObject("relay_info") +
+        identifiedObject("remote_controls") +
+        identifiedObject("remote_sources") +
+        regulatingCondEq("rotating_machines") +
+        powerSystemResource("series_compensators") +
+        identifiedObject("short_circuit_tests") +
+        identifiedObject("shunt_compensator_info") +
+        powerSystemResource("sites") +
+        regulatingCondEq("static_var_compensators") +
+        identifiedObject("streetlights") +
+        identifiedObject("sub_geographical_regions") +
+        powerSystemResource("substations") +
+        identifiedObject("switch_info") +
+        synchronousMachine("synchronous_machines") +
+        powerSystemResource("tap_changer_controls") +
+        document("tariffs") +
+        identifiedObject("terminals") +
+        identifiedObject("transformer_end_info") +
+        identifiedObject("transformer_star_impedances") +
+        identifiedObject("transformer_tank_info") +
+        usagePoint("usage_points") +
+        powerSystemResource("voltage_relays")
     ,
 
     // TODO: Every power system resource table needs the above.
     targetDatabases = setOf(DatabaseType.NETWORK_MODEL)
 )
 
-fun streetDetail(tableName: String): List<String> =
-    alterToNullableColumn(tableName, "building_name") +
-    alterToNullableColumn(tableName, "floor_identification") +
-    alterToNullableColumn(tableName, "street_name") +
-    alterToNullableColumn(tableName, "number") +
-    alterToNullableColumn(tableName, "suite_number") +
-    alterToNullableColumn(tableName, "type") +
-    alterToNullableColumn(tableName, "display_address")
-
 fun streetAddress(tableName: String): List<String> =
     alterToNullableColumn(tableName, "postal_code") +
         alterToNullableColumn(tableName, "po_box") +
-        streetDetail(tableName)
+        alterToNullableColumn(tableName, "building_name") +
+        alterToNullableColumn(tableName, "floor_identification") +
+        alterToNullableColumn(tableName, "street_name") +
+        alterToNullableColumn(tableName, "number") +
+        alterToNullableColumn(tableName, "suite_number") +
+        alterToNullableColumn(tableName, "type") +
+        alterToNullableColumn(tableName, "display_address")
 
 fun document(tableName: String): List<String> =
     identifiedObject(tableName) +
@@ -71,11 +159,11 @@ fun tapChanger(tableName: String): List<String> =
         alterToNullableColumn(tableName, "control_enabled")
 
 fun synchronousMachine(tableName: String): List<String> =
-    identifiedObject(tableName)+
+    identifiedObject(tableName) +
         alterToNullableColumn(tableName, "earthing")
 
 fun shuntCompensator(tableName: String): List<String> =
-    regulatingCondEq(tableName)+
+    regulatingCondEq(tableName) +
         alterToNullableColumn(tableName, "grounded")
 
 fun regulatingCondEq(tableName: String): List<String> =
@@ -102,7 +190,7 @@ fun usagePoint(tableName: String): List<String> =
         alterToNullableColumn(tableName, "is_virtual")
 
 fun pole(tableName: String): List<String> =
-    identifiedObject("poles") +
+    identifiedObject(tableName) +
         alterToNullableColumn(tableName, "classification")
 
 fun identifiedObject(tableName: String): List<String> =
@@ -111,8 +199,8 @@ fun identifiedObject(tableName: String): List<String> =
         "ALTER TABLE $tableName ADD COLUMN name TEXT",
         "UPDATE $tableName SET name = name_old",
 
-        "DROP INDEX ac_line_segments_name",
-        "CREATE INDEX ac_line_segments_name ON $tableName (name)",
+        "DROP INDEX ${tableName}_name",
+        "CREATE INDEX ${tableName}_name ON $tableName (name)",
         "ALTER TABLE $tableName DROP COLUMN name_old",
     ) + alterToNullableColumn(tableName, "description") +
         alterToNullableColumn(tableName, "num_diagram_objects")
@@ -152,4 +240,7 @@ private val `rename WindingConnection UNKNOWN_WINDING to UNKNOWN` = Change(
         "UPDATE transformer_end_info SET connection_kind = 'UNKNOWN' WHERE connection_kind = 'UNKNOWN_WINDING'",
     ),
     targetDatabases = setOf(DatabaseType.NETWORK_MODEL)
+)
+
+fun tablesToUpdate(): List<SqliteTable> = mutableListOf(
 )

b0bf5f226183e737ecb22642886476dc77e51e6b
Anthony Charlton <anthony.charlton@zepben.com>
2025-09-17 19:09:22 +1000

    Updated v61 change set tests to show what is actually changed.
    Signed-off-by: Anthony Charlton <anthony.charlton@zepben.com>

--- src/test/kotlin/com/zepben/ewb/database/sqlite/cim/upgrade/changesets/customer/ChangeSet61CustomerValidator.kt
+++ src/test/kotlin/com/zepben/ewb/database/sqlite/cim/upgrade/changesets/customer/ChangeSet61CustomerValidator.kt
@@ -8,78 +8,191 @@
 
 package com.zepben.ewb.database.sqlite.cim.upgrade.changesets.customer
 
+import com.zepben.ewb.database.getNullableString
 import com.zepben.ewb.database.paths.DatabaseType
 import com.zepben.ewb.database.sqlite.cim.upgrade.changesets.ChangeSetValidator
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.nullValue
 import java.sql.Statement
 
-// TODO: This needs all the data model changes verified
 object ChangeSet61CustomerValidator : ChangeSetValidator(DatabaseType.NETWORK_MODEL, 61) {
 
     //
-    // NOTE: In the validators we are only checking the columns that were actually changed.
+    // NOTE: We are utilising the SQLite feature of being able to put any type of data into a column by putting string into all fields. This stops us
+    //       having to deal with the complexity of column types in the validations, but still does the nullability checking.
+    //
+
+    //
+    // NOTE: Some columns were incorrectly left as `NOT NULL` in the v61 changeset, so the tests here will validate against thet. The fixes for these
+    //       columns have been added in change set 62 as we already had databases in the wild on v61 before this was detected.
     //
 
     override fun setUpStatements(): List<String> = listOf(
-        *`populate document` ("customer_agreements"),
-        *`populate identifiedObject` ("customers"),
-        *`populate document` ("pricing_structures"),
-        *`populate document` ("tariffs"),
+        "INSERT INTO customer_agreements (mrid, name, description, num_diagram_objects, title, created_date_time, author_name, type, status, comment, customer_mrid) VALUES ('mrid_1', 'name_1', 'description_1', 'num_diagram_objects_1', 'title_1', 'created_date_time_1', 'author_name_1', 'type_1', 'status_1', 'comment_1', 'customer_mrid_1');",
+        "INSERT INTO customers (mrid, name, description, num_diagram_objects, organisation_mrid, kind, num_end_devices, special_need) VALUES ('mrid_1', 'name_1', 'description_1', 'num_diagram_objects_1', 'organisation_mrid_1', 'kind_1', 'num_end_devices_1', 'special_need_1');",
+        "INSERT INTO organisations (mrid, name, description, num_diagram_objects) VALUES ('mrid_1', 'name_1', 'description_1', 'num_diagram_objects_1');",
+        "INSERT INTO pricing_structures (mrid, name, description, num_diagram_objects, title, created_date_time, author_name, type, status, comment) VALUES ('mrid_1', 'name_1', 'description_1', 'num_diagram_objects_1', 'title_1', 'created_date_time_1', 'author_name_1', 'type_1', 'status_1', 'comment_1');",
+        "INSERT INTO tariffs (mrid, name, description, num_diagram_objects, title, created_date_time, author_name, type, status, comment) VALUES ('mrid_1', 'name_1', 'description_1', 'num_diagram_objects_1', 'title_1', 'created_date_time_1', 'author_name_1', 'type_1', 'status_1', 'comment_1');",
     )
 
-    // There are no table changes, so no need to populate anything.
-    override fun populateStatements(): List<String> = emptyList()
+    override fun populateStatements(): List<String> = listOf(
+        "INSERT INTO customer_agreements (mrid, name, description, num_diagram_objects, title, created_date_time, author_name, type, status, comment, customer_mrid) VALUES ('mrid_2', null, null, null, null, null, null, null, null, 'should_be_nullable', null);",
+        "INSERT INTO customers (mrid, name, description, num_diagram_objects, organisation_mrid, kind, num_end_devices, special_need) VALUES ('mrid_2', null, null, null, null, 'kind_2', null, null);",
+        "INSERT INTO organisations (mrid, name, description, num_diagram_objects) VALUES ('mrid_2', 'should_be_nullable', 'should_be_nullable', 'should_be_nullable');",
+        "INSERT INTO pricing_structures (mrid, name, description, num_diagram_objects, title, created_date_time, author_name, type, status, comment) VALUES ('mrid_2', null, null, null, null, null, null, null, null, 'should_be_nullable');",
+        "INSERT INTO tariffs (mrid, name, description, num_diagram_objects, title, created_date_time, author_name, type, status, comment) VALUES ('mrid_2', null, null, null, null, null, null, null, null, 'should_be_nullable');",
+    )
 
     override fun validateChanges(statement: Statement) {
-        `validate document` (statement, "customer_agreements")
-        `validate identifiedObject` (statement, "customers")
-        `validate document` (statement, "pricing_structures")
-        `validate document` (statement, "tariffs")
+        `validate customer_agreements`(statement)
+        `validate customers`(statement)
+        `validate organisations`(statement)
+        `validate pricing_structures`(statement)
+        `validate tariffs`(statement)
     }
 
     override fun tearDownStatements(): List<String> =
-        listOf()
+        listOf(
+            "DELETE from customer_agreements;",
+            "DELETE from customers;",
+            "DELETE from organisations;",
+            "DELETE from pricing_structures;",
+            "DELETE from tariffs;",
+        )
 
-    private fun `populate document`(table: String) = arrayOf(
-        """
-           INSERT INTO $table (mrid, name, description, num_diagram_objects, title, author_name, type, status, comment) 
-           VALUES ('id1', 'name', 'desc', 1, 'title', 'probably_someone', 'definitely a type', 'some status', 'no comment')
-        """.trimIndent()
-    )
-
-    private fun `populate identifiedObject`(table: String) = arrayOf(
-        """
-           INSERT INTO $table (mrid, name, description, num_diagram_objects, kind) 
-           VALUES ('id1', 'name', 'desc', 1, 'nah, pretty mean')
-        """.trimIndent()
-    )
-
-    private fun `validate document`(statement: Statement, table: String) {
+    private fun `validate customer_agreements`(statement: Statement) {
         validateRows(
-            statement, "SELECT mrid, name, description, num_diagram_objects, title, author_name, type, status, comment FROM $table",
+            statement, "SELECT * FROM customer_agreements",
             { rs ->
-                assertThat(rs.getString("mrid"), equalTo("id1"))
-                assertThat(rs.getString("name"), equalTo("name"))
-                assertThat(rs.getString("description"), equalTo("desc"))
-                assertThat(rs.getInt("num_diagram_objects"), equalTo(1))
-                assertThat(rs.getString("title"), equalTo("title"))
-                assertThat(rs.getString("author_name"), equalTo("probably_someone"))
-                assertThat(rs.getString("type"), equalTo("definitely a type"))
-                assertThat(rs.getString("status"), equalTo("some status"))
-                assertThat(rs.getString("comment"), equalTo("no comment"))
+                assertThat(rs.getString("mrid"), equalTo("mrid_1"))
+                assertThat(rs.getString("name"), equalTo("name_1"))
+                assertThat(rs.getString("description"), equalTo("description_1"))
+                assertThat(rs.getString("num_diagram_objects"), equalTo("num_diagram_objects_1"))
+                assertThat(rs.getString("title"), equalTo("title_1"))
+                assertThat(rs.getString("created_date_time"), equalTo("created_date_time_1"))
+                assertThat(rs.getString("author_name"), equalTo("author_name_1"))
+                assertThat(rs.getString("type"), equalTo("type_1"))
+                assertThat(rs.getString("status"), equalTo("status_1"))
+                assertThat(rs.getString("comment"), equalTo("comment_1"))
+                assertThat(rs.getString("customer_mrid"), equalTo("customer_mrid_1"))
+            },
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_2"))
+                assertThat(rs.getNullableString("name"), nullValue())
+                assertThat(rs.getNullableString("description"), nullValue())
+                assertThat(rs.getNullableString("num_diagram_objects"), nullValue())
+                assertThat(rs.getNullableString("title"), nullValue())
+                assertThat(rs.getNullableString("created_date_time"), nullValue())
+                assertThat(rs.getNullableString("author_name"), nullValue())
+                assertThat(rs.getNullableString("type"), nullValue())
+                assertThat(rs.getNullableString("status"), nullValue())
+                assertThat(rs.getString("comment"), equalTo("should_be_nullable"))
+                assertThat(rs.getNullableString("customer_mrid"), nullValue())
             }
         )
     }
 
-    private fun `validate identifiedObject`(statement: Statement, table: String) {
+    private fun `validate customers`(statement: Statement) {
         validateRows(
-            statement, "SELECT mrid, name, description, num_diagram_objects FROM $table",
+            statement, "SELECT * FROM customers",
             { rs ->
-                assertThat(rs.getString("mrid"), equalTo("id1"))
-                assertThat(rs.getString("name"), equalTo("name"))
-                assertThat(rs.getString("description"), equalTo("desc"))
-                assertThat(rs.getInt("num_diagram_objects"), equalTo(1))
+                assertThat(rs.getString("mrid"), equalTo("mrid_1"))
+                assertThat(rs.getString("name"), equalTo("name_1"))
+                assertThat(rs.getString("description"), equalTo("description_1"))
+                assertThat(rs.getString("num_diagram_objects"), equalTo("num_diagram_objects_1"))
+                assertThat(rs.getString("organisation_mrid"), equalTo("organisation_mrid_1"))
+                assertThat(rs.getString("kind"), equalTo("kind_1"))
+                assertThat(rs.getString("num_end_devices"), equalTo("num_end_devices_1"))
+                assertThat(rs.getString("special_need"), equalTo("special_need_1"))
+            },
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_2"))
+                assertThat(rs.getNullableString("name"), nullValue())
+                assertThat(rs.getNullableString("description"), nullValue())
+                assertThat(rs.getNullableString("num_diagram_objects"), nullValue())
+                assertThat(rs.getNullableString("organisation_mrid"), nullValue())
+                assertThat(rs.getNullableString("kind"), equalTo("kind_2"))
+                assertThat(rs.getNullableString("num_end_devices"), nullValue())
+                assertThat(rs.getNullableString("special_need"), nullValue())
+            }
+        )
+    }
+
+    private fun `validate organisations`(statement: Statement) {
+        validateRows(
+            statement, "SELECT * FROM organisations",
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_1"))
+                assertThat(rs.getString("name"), equalTo("name_1"))
+                assertThat(rs.getString("description"), equalTo("description_1"))
+                assertThat(rs.getString("num_diagram_objects"), equalTo("num_diagram_objects_1"))
+            },
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_2"))
+                assertThat(rs.getString("name"), equalTo("should_be_nullable"))
+                assertThat(rs.getString("description"), equalTo("should_be_nullable"))
+                assertThat(rs.getString("num_diagram_objects"), equalTo("should_be_nullable"))
+            }
+        )
+    }
+
+    private fun `validate pricing_structures`(statement: Statement) {
+        validateRows(
+            statement, "SELECT * FROM pricing_structures",
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_1"))
+                assertThat(rs.getString("name"), equalTo("name_1"))
+                assertThat(rs.getString("description"), equalTo("description_1"))
+                assertThat(rs.getString("num_diagram_objects"), equalTo("num_diagram_objects_1"))
+                assertThat(rs.getString("title"), equalTo("title_1"))
+                assertThat(rs.getString("created_date_time"), equalTo("created_date_time_1"))
+                assertThat(rs.getString("author_name"), equalTo("author_name_1"))
+                assertThat(rs.getString("type"), equalTo("type_1"))
+                assertThat(rs.getString("status"), equalTo("status_1"))
+                assertThat(rs.getString("comment"), equalTo("comment_1"))
+            },
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_2"))
+                assertThat(rs.getNullableString("name"), nullValue())
+                assertThat(rs.getNullableString("description"), nullValue())
+                assertThat(rs.getNullableString("num_diagram_objects"), nullValue())
+                assertThat(rs.getNullableString("title"), nullValue())
+                assertThat(rs.getNullableString("created_date_time"), nullValue())
+                assertThat(rs.getNullableString("author_name"), nullValue())
+                assertThat(rs.getNullableString("type"), nullValue())
+                assertThat(rs.getNullableString("status"), nullValue())
+                assertThat(rs.getString("comment"), equalTo("should_be_nullable"))
+            }
+        )
+    }
+
+    private fun `validate tariffs`(statement: Statement) {
+        validateRows(
+            statement, "SELECT * FROM tariffs",
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_1"))
+                assertThat(rs.getString("name"), equalTo("name_1"))
+                assertThat(rs.getString("description"), equalTo("description_1"))
+                assertThat(rs.getString("num_diagram_objects"), equalTo("num_diagram_objects_1"))
+                assertThat(rs.getString("title"), equalTo("title_1"))
+                assertThat(rs.getString("created_date_time"), equalTo("created_date_time_1"))
+                assertThat(rs.getString("author_name"), equalTo("author_name_1"))
+                assertThat(rs.getString("type"), equalTo("type_1"))
+                assertThat(rs.getString("status"), equalTo("status_1"))
+                assertThat(rs.getString("comment"), equalTo("comment_1"))
+            },
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_2"))
+                assertThat(rs.getNullableString("name"), nullValue())
+                assertThat(rs.getNullableString("description"), nullValue())
+                assertThat(rs.getNullableString("num_diagram_objects"), nullValue())
+                assertThat(rs.getNullableString("title"), nullValue())
+                assertThat(rs.getNullableString("created_date_time"), nullValue())
+                assertThat(rs.getNullableString("author_name"), nullValue())
+                assertThat(rs.getNullableString("type"), nullValue())
+                assertThat(rs.getNullableString("status"), nullValue())
+                assertThat(rs.getString("comment"), equalTo("should_be_nullable"))
             }
         )
     }
--- src/test/kotlin/com/zepben/ewb/database/sqlite/cim/upgrade/changesets/diagram/ChangeSet61DiagramValidator.kt
+++ src/test/kotlin/com/zepben/ewb/database/sqlite/cim/upgrade/changesets/diagram/ChangeSet61DiagramValidator.kt
@@ -8,77 +8,107 @@
 
 package com.zepben.ewb.database.sqlite.cim.upgrade.changesets.diagram
 
+import com.zepben.ewb.database.getNullableString
 import com.zepben.ewb.database.paths.DatabaseType
 import com.zepben.ewb.database.sqlite.cim.upgrade.changesets.ChangeSetValidator
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.nullValue
 import java.sql.Statement
 
 object ChangeSet61DiagramValidator : ChangeSetValidator(DatabaseType.NETWORK_MODEL, 61) {
 
     //
-    // NOTE: In the validators we are only checking the columns that were actually changed.
+    // NOTE: We are utilising the SQLite feature of being able to put any type of data into a column by putting string into all fields. This stops us
+    //       having to deal with the complexity of column types in the validations, but still does the nullability checking.
     //
 
     override fun setUpStatements(): List<String> = listOf(
-        *`populate identifiedObjectRotation` ("diagram_objects"),
-        *`populate identifiedObject` ("diagrams"),
-        *`populate diagram object points`()
+        "INSERT INTO diagram_object_points (diagram_object_mrid, sequence_number, x_position, y_position) VALUES ('diagram_object_mrid_1', 'sequence_number_1', null, null);",
+        "INSERT INTO diagram_objects (mrid, name, description, num_diagram_objects, identified_object_mrid, diagram_mrid, style, rotation) VALUES ('mrid_1', 'name_1', 'description_1', 'num_diagram_objects_1', 'identified_object_mrid_1', 'diagram_mrid_1', 'style_1', 'rotation_1');",
+        "INSERT INTO diagrams (mrid, name, description, num_diagram_objects, diagram_style, orientation_kind) VALUES ('mrid_1', 'name_1', 'description_1', 'num_diagram_objects_1', 'diagram_style_1', 'orientation_kind_1');",
     )
 
-    // There are no table changes, so no need to populate anything.
-    override fun populateStatements(): List<String> = emptyList()
+    override fun populateStatements(): List<String> = listOf(
+        "INSERT INTO diagram_object_points (diagram_object_mrid, sequence_number, x_position, y_position) VALUES ('diagram_object_mrid_2', 'sequence_number_2', 'x_position_2', 'y_position_2');",
+        "INSERT INTO diagram_objects (mrid, name, description, num_diagram_objects, identified_object_mrid, diagram_mrid, style, rotation) VALUES ('mrid_2', null, null, null, 'identified_object_mrid_2', 'diagram_mrid_2', null, 'rotation_2');",
+        "INSERT INTO diagrams (mrid, name, description, num_diagram_objects, diagram_style, orientation_kind) VALUES ('mrid_2', null, null, null, 'diagram_style_2', 'orientation_kind_2');",
+    )
 
     override fun validateChanges(statement: Statement) {
-        `validate identifiedObject` (statement, "diagram_objects")
-        `validate identifiedObject` (statement, "diagrams")
-        `validate diagram object points`(statement)
+        `validate diagram_object_points`(statement)
+        `validate diagram_objects`(statement)
+        `validate diagrams`(statement)
     }
 
-    override fun tearDownStatements(): List<String> =
-        listOf()
-
-    private fun `populate identifiedObjectRotation`(table: String) = arrayOf(
-        """
-           INSERT INTO $table (mrid, name, description, num_diagram_objects, rotation) 
-           VALUES ('id1', 'name', 'desc', 1, 'fully')
-        """.trimIndent()
+    override fun tearDownStatements(): List<String> = listOf(
+        "DELETE FROM diagram_object_points;",
+        "DELETE FROM diagram_objects;",
+        "DELETE FROM diagrams;",
     )
 
-    private fun `populate identifiedObject`(table: String) = arrayOf(
-        """
-           INSERT INTO $table (mrid, name, description, num_diagram_objects, diagram_style, orientation_kind) 
-           VALUES ('id1', 'name', 'desc', 1, 'stylish', 'first year')
-        """.trimIndent()
-    )
-
-    private fun `populate diagram object points`() = arrayOf(
-        """
-           INSERT INTO diagram_object_points (diagram_object_mrid, sequence_number, x_position, y_position)
-           VALUES ('id1', 1, 0.0, 0.0)
-        """.trimIndent()
-    )
-
-    private fun `validate identifiedObject`(statement: Statement, table: String) {
+    private fun `validate diagram_object_points`(statement: Statement) {
         validateRows(
-            statement, "SELECT mrid, name, description, num_diagram_objects FROM $table",
+            statement, "SELECT * FROM diagram_object_points",
             { rs ->
-                assertThat(rs.getString("mrid"), equalTo("id1"))
-                assertThat(rs.getString("name"), equalTo("name"))
-                assertThat(rs.getString("description"), equalTo("desc"))
-                assertThat(rs.getInt("num_diagram_objects"), equalTo(1))
+                assertThat(rs.getString("diagram_object_mrid"), equalTo("diagram_object_mrid_1"))
+                assertThat(rs.getString("sequence_number"), equalTo("sequence_number_1"))
+                assertThat(rs.getNullableString("x_position"), nullValue())
+                assertThat(rs.getNullableString("y_position"), nullValue())
+            },
+            { rs ->
+                assertThat(rs.getString("diagram_object_mrid"), equalTo("diagram_object_mrid_2"))
+                assertThat(rs.getString("sequence_number"), equalTo("sequence_number_2"))
+                assertThat(rs.getString("x_position"), equalTo("x_position_2"))
+                assertThat(rs.getString("y_position"), equalTo("y_position_2"))
             }
         )
     }
 
-    private fun `validate diagram object points`(statement: Statement) {
+    private fun `validate diagram_objects`(statement: Statement) {
         validateRows(
-            statement, "SELECT diagram_object_mrid, sequence_number, x_position, y_position FROM diagram_object_points",
+            statement, "SELECT * FROM diagram_objects",
             { rs ->
-                assertThat(rs.getString("diagram_object_mrid"), equalTo("id1"))
-                assertThat(rs.getInt("sequence_number"), equalTo(1))
-                assertThat(rs.getDouble("x_position"), equalTo(0.0))
-                assertThat(rs.getDouble("y_position"), equalTo(0.0))
+                assertThat(rs.getString("mrid"), equalTo("mrid_1"))
+                assertThat(rs.getString("name"), equalTo("name_1"))
+                assertThat(rs.getString("description"), equalTo("description_1"))
+                assertThat(rs.getString("num_diagram_objects"), equalTo("num_diagram_objects_1"))
+                assertThat(rs.getString("identified_object_mrid"), equalTo("identified_object_mrid_1"))
+                assertThat(rs.getString("diagram_mrid"), equalTo("diagram_mrid_1"))
+                assertThat(rs.getString("style"), equalTo("style_1"))
+                assertThat(rs.getString("rotation"), equalTo("rotation_1"))
+            },
+            { rs ->
+                assertThat(rs.getNullableString("mrid"), equalTo("mrid_2"))
+                assertThat(rs.getNullableString("name"), nullValue())
+                assertThat(rs.getNullableString("description"), nullValue())
+                assertThat(rs.getNullableString("num_diagram_objects"), nullValue())
+                assertThat(rs.getString("identified_object_mrid"), equalTo("identified_object_mrid_2"))
+                assertThat(rs.getString("diagram_mrid"), equalTo("diagram_mrid_2"))
+                assertThat(rs.getNullableString("style"), nullValue())
+                assertThat(rs.getString("rotation"), equalTo("rotation_2"))
+            }
+        )
+    }
+
+    private fun `validate diagrams`(statement: Statement) {
+        validateRows(
+            statement, "SELECT * FROM diagrams",
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_1"))
+                assertThat(rs.getString("name"), equalTo("name_1"))
+                assertThat(rs.getString("description"), equalTo("description_1"))
+                assertThat(rs.getString("num_diagram_objects"), equalTo("num_diagram_objects_1"))
+                assertThat(rs.getString("diagram_style"), equalTo("diagram_style_1"))
+                assertThat(rs.getString("orientation_kind"), equalTo("orientation_kind_1"))
+            },
+            { rs ->
+                assertThat(rs.getString("mrid"), equalTo("mrid_2"))
+                assertThat(rs.getNullableString("name"), nullValue())
+                assertThat(rs.getNullableString("description"), nullValue())
+                assertThat(rs.getNullableString("num_diagram_objects"), nullValue())
+                assertThat(rs.getString("diagram_style"), equalTo("diagram_style_2"))
+                assertThat(rs.getString("orientation_kind"), equalTo("orientation_kind_2"))
             }
         )
     }
